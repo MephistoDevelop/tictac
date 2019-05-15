a60dbbf298246931652aca7870b28d3d547acf84
Diego Robles <diego@hemployeed.com>
2019-05-15 16:41:21 -0400

    fix(board):fix new game and show winner

--- lib/board.rb
+++ lib/board.rb
@@ -5,13 +5,13 @@ class Board
     @playing = true
     @plays = {}
     @winner = false
-    @last_play={}
+    @@last_play={}
   end
   def play(square)
     if(@plays[square] == nil)
       @plays[square] = @plays.length == 0 || (@plays.length + 1)%2 != 0
-      @last_play={}
-      @last_play[square]=@plays[square]
+      @@last_play={}
+      @@last_play[square] = @plays[square]
     else
       false
     end
@@ -21,11 +21,13 @@ class Board
   def get_plays
       return @plays
   end
+  def get_last_play
+    @@last_play.values[0]
+  end
   def get_active
     return @plays.length == 0 || (@plays.length + 1)%2 != 0
   end
   def check_horizontal
-    @winner = false
     for i in [1,4,7] do
       if @plays[i] != nil
         if(@plays[i] == @plays[i+1] && @plays[i+1] == @plays[i+2])
@@ -34,46 +36,44 @@ class Board
         end
       end
     end
-    winner
-end
-def check_vertical
-  @winner = false
-  for i in [1,2,3] do
-    if(@plays[i] != nil)
-    if (@plays[i] == @plays[i+3] && @plays[i+3] == @plays[i+6])
-      @winner = true
-      break if @winner
+    @winner
+  end
+  def check_vertical
+    for i in [1,2,3] do
+      if(@plays[i] != nil)
+      if (@plays[i] == @plays[i+3] && @plays[i+3] == @plays[i+6])
+        @winner = true
+        break if @winner
+      end
     end
+    end
+    @winner
   end
-  end
-  winner
-end
-def check_cross
-  winner = false
-  if(@plays[1] != nil && @plays[3] != nil && @plays[5] != nil || @plays[7] != nil && @plays[9] != nil )
-    if (@plays[1] ==@plays[5] && @plays[1] ==@plays[9] )
+  def check_cross
+    if(@plays[1] != nil && @plays[3] != nil && @plays[5] != nil || @plays[7] != nil && @plays[9] != nil )
+      if (@plays[1] ==@plays[5] && @plays[1] ==@plays[9] )
+        @winner = true
+        puts ""
+        @playing = false
+      end
+      if (@plays[3] == @plays[5] && @plays[3] == @plays[7] )
       @winner = true
       puts ""
       @playing = false
+      end
     end
-    if (@plays[3] == @plays[5] && @plays[3] == @plays[7] )
-    winner = true
-    puts ""
+    @winner
+  end
+  def check_draw
+    if (@plays.length == 9 && @winner==false)
     @playing = false
+    @draw=true
+    true
+    else
+      false
     end
   end
-  winner
-end
-def check_draw
-  if (@plays.length == 9 && @winner==false)
-   @playing = false
-   @draw=true
-   true
-  else
-    false
+  def check_winner
+    return check_horizontal ||  check_vertical || check_cross
   end
-end
-def check_winner
-   return check_horizontal ||  check_vertical || check_cross
-end
 end
