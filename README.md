7f6a969be27d4c6fb230f25fac9dbd0f7821f281
Cristian <inesherald@gmail.com>
2019-05-21 09:19:59 -0500

    ref(development):fix last code review

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# Ruby Projects
+#Tic Tac Toe Game
 
-### Basic Guide for Ruby languaje
+### Basic Ruby Repository
 
 **Description**
 
-Simple repository to save basic projects with the basic structure for Ruby languaje.
+Simple repository in ruby language to play a simple Tic-Tac-Toe Game
 
 # Languaje
 
 Ruby
+
+#Instruction
+
+To play the game, run `ruby ~/bin/main` on your terminal.
 
 **Coded by**
 
--- lib/board.rb
+++ lib/board.rb
@@ -41,27 +41,23 @@ class Board
     plays = merge_plays
     arr_player=[]
     arr_player2=[]
-        plays.each{|x,y|
-          if(y=='⭕️')
-            arr_player2.push(x)
+    plays.each{|x,y|
+        if(y=='⭕️')
+          arr_player2.push(x)
           end }
-          plays.each{|x,y|
-
-            if(y=='❌')
-              arr_player.push(x)
-            end }
-  if get_plays_count > 0
+    plays.each{ |x,y|
+      if(y=='❌')
+        arr_player.push(x)
+         end }
+    if get_plays_count > 0
     @win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     @win.each{|array|
      if arr_player.size > 2
         sort_player = (arr_player & array).sort
-        #puts "jugador 1 gana"
       end
       if arr_player2.size > 2
         sort_player2 = (arr_player2 & array).sort
-        #puts "jugador 2 gana"
       end
-      # puts "Esto son win pathern #{array}: y esto es player1 #{  arr_player } : player2 #{ arr_player2 } \n sort_player : #{sort_player}: sort_player2 : #{sort_player2} "
       if (array==sort_player || array==sort_player2 )
         return true
       end
@@ -69,7 +65,6 @@ class Board
     end
       return false
   end
-
   def winner
     return @player2.name if get_plays_count % 2 == 0
     @player1.name
