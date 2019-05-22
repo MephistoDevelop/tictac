02eaf06b1e649208fd247188fb5f3e97ff85adda
Cristian <inesherald@gmail.com>
2019-05-22 13:08:25 -0500

    ref(testing):add vertical,cross winner and get_counts from plays test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Board do
   end
 end
 
-  describe "#check_horizontalwinner" do
+  describe "#check_horizontal_winner" do
     it "check if some player win at top" do
       player1 =Player.new('❌')
       player2 =Player.new('⭕️')
@@ -31,6 +31,48 @@ end
       player2.make_play(4)
       player1.make_play(3)
       expect(board.check_winner).to eql(true)
+    end
+  end
+
+  describe "#check_vertical_winner" do
+    it "check if some player win at vertical" do
+      player1 =Player.new('❌')
+      player2 =Player.new('⭕️')
+      board = Board.new(player1,player2)
+      player1.make_play(1)
+      player2.make_play(5)
+      player1.make_play(4)
+      player2.make_play(3)
+      player1.make_play(7)
+      expect(board.check_winner).to eql(true)
+    end
+  end
+
+  describe "#check_cross_winner" do
+    it "check if some player win at cross" do
+      player1 =Player.new('❌')
+      player2 =Player.new('⭕️')
+      board = Board.new(player1,player2)
+      player1.make_play(1)
+      player2.make_play(3)
+      player1.make_play(5)
+      player2.make_play(8)
+      player1.make_play(9)
+      expect(board.check_winner).to eql(true)
+    end
+  end
+
+  describe "#get_plays_count" do
+    it "check the number of plays of each player" do
+      player1 =Player.new('❌')
+      player2 =Player.new('⭕️')
+      board = Board.new(player1,player2)
+      player1.make_play(1)
+      player2.make_play(3)
+      player1.make_play(5)
+      player2.make_play(8)
+      player1.make_play(9)
+      expect(board.get_plays_count).to eql(5)
     end
   end
 
