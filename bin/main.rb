003d65a4775d8db5d66785474b17526d9222f779
Cristian <inesherald@gmail.com>
2020-01-14 04:46:12 -0600

    User Interface Creation

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,13 @@
-puts"Hello world"
+require_relative "./ui"
+require_relative "../lib/player"
+
+ui = UI.new
+
+while ui.new_game? do
+  player1 = Player.new("❌")
+  player2 = Player.new("⭕️")
+  ui.start
+end
+
+ui.quit
+
