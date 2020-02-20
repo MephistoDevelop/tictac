fee2013fbe1b284952883460a62dff01f3284c1d
MephistoDevelop <inesherald@gmail.com>
2020-02-20 02:07:04 -0600

    Fix some rubocop problems

--- lib/board.rb
+++ lib/board.rb
@@ -1,80 +1,86 @@
+# frozen_string_literal: true
+
 class Board
   attr_accessor :player1
   attr_accessor :player2, :plays
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @@plays = {}
+    @plays = {}
   end
+
   def check_play(square)
-    return true if @@plays[square]
+    return true if @plays[square]
+
     false
   end
+
   def play(square)
-    if ((get_plays_count + 1) % 2 != 0)
-    make_play(square)
-    else
-    make_play(square)
-    end
+    make_play(square) if (make_plays_count + 1).odd?
   end
+
   def playing
-    return false if check_winner || draw?
+    return false if winner? || draw?
+
     true
   end
+
   def draw?
-    case get_plays_count
+    case make_plays_count
     when 9
-      return true if !check_winner
-      false
-    else
+      return true unless winner?
+
       false
     end
   end
-  def get_plays_count
-    @@plays.length
+
+  def make_plays_count
+    @plays.length
   end
-  def check_winner
-    arr_player=[]
-    arr_player2=[]
-    @@plays.each{|x,y|
-        if(y=='⭕️')
-          arr_player2.push(x)
-        elsif(y=='❌')
-            arr_player.push(x)
-             end
-        }
-    if get_plays_count > 0
-    @win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-    @win.each{|array|
-     if arr_player.size > 2
-        sort_player = (arr_player & array).sort
+
+  def winner?
+    arr_player = []
+    arr_player2 = []
+    @plays.each do |x, y|
+      if y == '⭕️'
+        arr_player2.push(x)
+      elsif y == '❌'
+        arr_player.push(x)
       end
-      if arr_player2.size > 2
-        sort_player2 = (arr_player2 & array).sort
-      end
-      if (array==sort_player || array==sort_player2 )
-        return true
-      end
-      }
     end
-      return false
+    if make_plays_count.positive?
+      @win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+      @win.each do |array|
+        sort_player = (arr_player & array).sort if arr_player.size > 2
+        sort_player2 = (arr_player2 & array).sort if arr_player2.size > 2
+        return true if array == sort_player || array == sort_player2
+      end
+    end
+    false
   end
+
   def winner
-    return @player2.name if get_plays_count % 2 == 0
+    return @player2.name if make_plays_count.even?
+
     @player1.name
   end
-  def make_play(square)
-    if (get_plays_count % 2 == 0)
-      @name = '❌'
+
+  def check_player
+    if (make_plays_count + 1 % 2).zero?
+      '❌'
     else
-      @name = '⭕️'
+      '⭕️'
     end
-    @@plays[square] = @name
   end
-  def plays
-    @@plays
-  end
-  def set_plays(plays)
-    @@plays = plays
+
+  def make_play(square)
+    @name = if make_plays_count.even?
+              '❌'
+            else
+              '⭕️'
+            end
+    @plays[square] = @name
+
+    puts "Player #{@name} Turn"
   end
 end
